04a2f1250aaad0ce05c17d01f7befc1d0d08c0ab
Rivenshell Salomon <rivenshell111@gmail.com>
2023-08-27 18:45:30 -0400

    structural setup

---
+++ app/Components/Navbar.tsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Navbar() {
+  return (
+    <>
+      <nav>
+        <li>
+          <a>Information</a>
+          <a>Privacy Policy</a>
+          <a>Product</a>
+        </li>
+      </nav>
+    </>
+  )
+}
